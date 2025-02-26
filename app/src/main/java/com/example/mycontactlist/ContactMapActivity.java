54d10dbaa8e985e0a1b30786f238bdd919cb19fe
Ian McIntosh <ianamcintosh108@gmail.com>
2025-02-26 15:12:18 -0500

    Progress Commit

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -1,9 +1,17 @@
 package com.example.mycontactlist;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +30,9 @@ import java.io.IOException;
 import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener gpsListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +91,7 @@ public class ContactMapActivity extends AppCompatActivity {
         Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
         locationButton.setOnClickListener(new View.OnClickListener(){
 
+            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View v){
                 EditText editAddress = (EditText) findViewById(R.id.editAddress);
@@ -94,12 +106,36 @@ public class ContactMapActivity extends AppCompatActivity {
 
                 List<Address> addresses = null;
                 Geocoder geo = new Geocoder(ContactMapActivity.this);
-                try{
-                    addresses = geo.getFromLocationName(address, 1);
+                try {
+                    locationManager = (LocationManager) getBaseContext()
+                            .getSystemService(Context.LOCATION_SERVICE);
+
+                    gpsListener = new LocationListener() {
+                        public void onLocationChanged(Location location) {
+                            TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                            TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+                            TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
+
+                            txtLatitude.setText(String.valueOf(location.getLatitude()));
+                            txtLongitude.setText(String.valueOf(location.getLongitude()));
+                            txtAccuracy.setText(String.valueOf(location.getAccuracy()));
+                        }
+
+                        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                        public void onProviderEnabled(String provider) {}
+
+                        public void onProviderDisabled(String provider) {}
+                    };
+
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER, 0, 0, gpsListener
+                    );
+                } catch (Exception e) {
+                    Toast.makeText(getBaseContext(), "Error, Location not available",
+                            Toast.LENGTH_LONG).show();
                 }
-                catch(IOException e){
-                    e.printStackTrace();
-                }
+
 
                 TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
                 TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
@@ -108,6 +144,60 @@ public class ContactMapActivity extends AppCompatActivity {
                 txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
             }
         });
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        try{
+            locationManager.removeUpdates(gpsListener);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void startLocationUpdates(){
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        try {
+            locationManager = (LocationManager) getBaseContext()
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+            gpsListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                    TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+                    TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
+
+                    txtLatitude.setText(String.valueOf(location.getLatitude()));
+                    txtLongitude.setText(String.valueOf(location.getLongitude()));
+                    txtAccuracy.setText(String.valueOf(location.getAccuracy()));
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                public void onProviderEnabled(String provider) {}
+
+                public void onProviderDisabled(String provider) {}
+            };
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 0, 0, gpsListener
+            );
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), "Error, Location not available",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
 
